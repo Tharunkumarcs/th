523ec4a7608deaf4e7498583cbdcc820b39082b7
Tharunkumarcs <47142208+Tharunkumarcs@users.noreply.github.com>
2019-02-11 16:16:27 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a<0)
+{
+printf("\n it is invalid");
+}
+if(a%2==0)
+{
+printf("\n it is even");
+}
+else
+{
+pirntf(\n it is odd");
+}
+getch();
+}
